f32bbd8d78d21a12b1ae5bc092b7dde007d6b692
Julian Lastra <julian.lastra@tsoftglobal.com>
2020-10-12 18:35:45 -0300

    Agregar archivos de proyecto.

---
+++ ElasticSearch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ElasticSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bot integration");
+
+            Requests r = new Requests();
+            r.url = "http://localhost:9200/";
+            r.response = r.get();
+            Console.WriteLine(r.response);
+        }
+
+    }
+}
---
+++ ElasticSearch/Requests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+
+namespace ElasticSearch
+{
+    class Requests
+    {
+        public string url { get; set; }
+        public string response { get; set; }
+
+        public string get()
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    response = client.DownloadString(url);
+                }
+            }
+            catch (Exception e)
+            {
+                response = "Error in request" + e;
+            }
+
+            return response;
+        }
+
+        public string post()
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.Headers[HttpRequestHeader.ContentType] = "application/json";
+                    
+                    response = client.DownloadString(url);
+                }
+            }
+            catch (Exception e)
+            {
+                response = "Error in request" + e;
+            }
+
+            return response;
+        }
+    }
+}
