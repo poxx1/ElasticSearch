54d7305160cd8326f203d36be7bcc2a426d1f4df
poxx1 <julianlastrama@outlook.com>
2020-10-12 18:48:17 -0300

    Release test

--- ElasticSearch/Program.cs
+++ ElasticSearch/Program.cs
@@ -6,7 +6,13 @@ namespace ElasticSearch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bot integration");
+            #region Intro Screen
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("                                             REST API ELASTIC SEARCH");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("                                                                                                                        \r\n\r\n");
+            Console.BackgroundColor = ConsoleColor.Black;
+            #endregion
 
             Requests r = new Requests();
             r.url = "http://localhost:9200/";
