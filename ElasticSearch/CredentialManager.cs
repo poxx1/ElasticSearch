11e9f1247d597232aa2a5a43965f05bce8cbdadb
poxx1 <julianlastrama@outlook.com>
2020-11-18 11:41:51 -0300

    Updates de seguridad. Crypter y App.Config actualizados

---
+++ ElasticSearch/CredentialManager.cs
@@ -0,0 +1,32 @@
+﻿using CredentialManagement;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElasticSearch
+{
+    class CredentialManager
+    {
+        public string Username { get; set; }
+        public string Password { get; set;}
+
+        protected Credential cd = new Credential();
+
+        public void ElasticSearch()
+        {
+            cd.Target = "elastic";
+            cd.Load();
+            Username = cd.Username;
+            Password = cd.Password;
+        }
+
+        public void Kibana()
+        {
+            cd.Target = "kibana";
+            cd.Load();
+            Username = cd.Username;
+            Password = cd.Password;
+        }
+    }
+}
+
---
+++ ElasticSearch/Crypter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ElasticSearch
+{
+    class Crypter
+    {
+        public string MD5Gen(string c)
+        {
+            var code = new UnicodeEncoding();
+            var md5 = new MD5CryptoServiceProvider();
+            byte[] ByteS = code.GetBytes(c);
+            byte[] Hash = md5.ComputeHash(ByteS);
+
+            return Hash.ToString();
+        }
+
+        public string Encriptar(string c)
+        {
+            //A la key la puedo poner en el .config o usar el crendential manager.
+            string key = "asd";
+            byte[] keyArray;
+            byte[] cifrar = UTF8Encoding.UTF8.GetBytes(c);
+
+            try
+            {
+                var md5 = new MD5CryptoServiceProvider();
+                var t = new TripleDESCryptoServiceProvider();
+
+                keyArray = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                md5.Clear();
+
+                t.Key = keyArray;
+                t.Mode = CipherMode.ECB;
+                t.Padding = PaddingMode.PKCS7;
+                ICryptoTransform e = t.CreateEncryptor();
+                byte[] resultado = e.TransformFinalBlock(cifrar, 0, cifrar.Length);
+                t.Clear();
+
+                c = Convert.ToBase64String(resultado, 0, resultado.Length);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return c;
+        }
+
+        public string Desencriptar(string c)
+        {
+            //A la key la puedo poner en el .config o usar el crendential manager.
+            string key = "asd";
+
+            try
+            {
+                byte[] keyArray;
+                byte[] decifrar = Convert.FromBase64String(c);
+
+                var md5 = new MD5CryptoServiceProvider();
+                var t = new TripleDESCryptoServiceProvider();
+
+                keyArray = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                md5.Clear();
+
+                t.Key = keyArray;
+                t.Mode = CipherMode.ECB;
+                t.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform e = t.CreateDecryptor();
+                byte[] resultado = e.TransformFinalBlock(decifrar, 0, decifrar.Length);
+                t.Clear();
+
+                c = UTF8Encoding.UTF8.GetString(resultado);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return c;
+        }
+    }
+}
